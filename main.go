bdf984f4a92cff75ea7886ee61f2cac0ff3ff879
Michał Darda <michaldarda@gmail.com>
2017-01-05 07:32:24 +0100

    Split indexer into separate file

--- main.go
+++ main.go
@@ -8,34 +8,6 @@ import (
 	"strings"
 )
 
-type WordOccurrence struct {
-	documentNumber int
-	wordNumber     int
-}
-type InvertedIndex map[string][]WordOccurrence
-
-func (index InvertedIndex) Add(word string, documentNumber int, wordNumber int) {
-	if index[word] != nil {
-		index[word] = append(index[word], WordOccurrence{documentNumber: documentNumber, wordNumber: wordNumber})
-		return
-	}
-	index[word] = []WordOccurrence{WordOccurrence{documentNumber: documentNumber, wordNumber: wordNumber}}
-}
-
-func (index InvertedIndex) PrettyPrint() {
-	for word, occurrences := range index {
-		fmt.Print(word)
-		fmt.Print(" {")
-
-		for _, occurence := range occurrences {
-			fmt.Printf("(%d, %d),", occurence.documentNumber, occurence.wordNumber)
-		}
-		fmt.Print("}")
-
-		fmt.Println("")
-	}
-}
-
 func main() {
 	argsCount := len(os.Args)
 	if argsCount < 2 {
