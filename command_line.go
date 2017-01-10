6f476bd3dd40272404efb5849db81f980aa7466c
Michał Darda <michal.darda@kontomatik.com>
2017-01-10 11:22:16 +0100

    Support for word doc_num:word_num format

--- command_line.go
+++ command_line.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -15,6 +16,7 @@ func startCommandLine(index InvertedIndex, device io.Reader) {
 	reader := bufio.NewReader(device)
 	for {
 		fmt.Print("> ")
+
 		text, _ := reader.ReadString('\n')
 		if text == "!\n" {
 			os.Exit(0)
@@ -35,7 +37,33 @@ func startCommandLine(index InvertedIndex, device io.Reader) {
 			fmt.Println(currentWord, result)
 			currentOccurrence = result
 		} else {
-			currentWord = strings.TrimRight(text, "\n")
+			// move up
+			command := strings.TrimRight(text, "\n")
+			tokenizedCommand := strings.FieldsFunc(command, func(r rune) bool {
+				return r == ' ' || r == ':'
+			})
+
+			if len(tokenizedCommand) != 1 && len(tokenizedCommand) != 3 {
+				fmt.Println("Wrong command", len(tokenizedCommand))
+				continue
+			}
+
+			currentWord = tokenizedCommand[0]
+			if len(tokenizedCommand) == 3 {
+				documentNumber, err := strconv.Atoi(tokenizedCommand[1])
+				if err != nil {
+					fmt.Println("Supply command in format word doc_num:word_num")
+				}
+				wordNumber, err := strconv.Atoi(tokenizedCommand[2])
+				if err != nil {
+					fmt.Println("Supply command in format word doc_num:word_num")
+				}
+
+				currentOccurrence = WordOccurrence{documentNumber: documentNumber, wordNumber: wordNumber}
+			} else {
+				currentOccurrence = WordOccurrence{documentNumber: -1, wordNumber: -1}
+			}
+			fmt.Println("searching for command greater than", currentOccurrence)
 			currentOccurrence = index.SearchForward(currentWord, currentOccurrence)
 
 			fmt.Println(currentWord, currentOccurrence)
